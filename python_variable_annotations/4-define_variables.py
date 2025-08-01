8534dad904356b5d85c8458df44a4930f9ef1ccc
voicedhealer <vivien.bernardot@gmail.com>
2025-08-01 13:54:37 +0200

    exercice 4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Module qui définit et annote des variables spécifiques."""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
