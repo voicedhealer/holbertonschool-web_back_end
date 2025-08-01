a25cfdf40ef90773fc22a2f7321e63350be2a01c
voicedhealer <vivien.bernardot@gmail.com>
2025-08-01 14:13:12 +0200

    exercice 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""Module qui annote une fonction utilisant le duck typing sur des itérables."""
+"""Module duck typing sur des itérables."""
 
 from typing import Iterable, Sequence, List, Tuple, Any
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Retourne une liste de tuples contenant chaque élément de lst et sa longueur.
+    Retourne une liste de tuples contenant chaque élément.
     """
     return [(i, len(i)) for i in lst]
