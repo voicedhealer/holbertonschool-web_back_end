1c1b161d2fd6fdd5c5bdc45ce086ed2d4c25f700
voicedhealer <vivien.bernardot@gmail.com>
2025-08-01 13:26:27 +0200

    update exercice 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Module qui contient une fonction d'addition avec annotations de type."""
+
+
 def add(a: float, b: float) -> float:
     """Additionne deux float et retourne le résultat."""
     return a + b
