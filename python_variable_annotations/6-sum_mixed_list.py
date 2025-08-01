357daf035251ec95103d598a845fc8a37abc0d6c
voicedhealer <vivien.bernardot@gmail.com>
2025-08-01 14:02:17 +0200

    exercice 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""Module qui définit sum_mixed_list, liste mixte d'int et de float."""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Retourne la somme des éléments (int & float) d'une liste, en float."""
+    return float(sum(mxd_lst))
