69dbb744fb8b0a3d6b760d929ade102aef207ffb
voicedhealer <vivien.bernardot@gmail.com>
2025-08-01 13:41:00 +0200

    exercice 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Module qui contient une fonction floor qui annote les types."""
+
+import math
+
+def floor(n: float) -> int:
+    """Retourne la partie entière (floor) d'un nombre flottant."""
+    return math.floor(n)
