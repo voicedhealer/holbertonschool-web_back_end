d71ce539d0eb855a15ffb238b22211ee18fc84f1
voicedhealer <vivien.bernardot@gmail.com>
2025-08-01 13:59:18 +0200

    exercice 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""Module qui définit une fonction sum_list qui somme une liste de floats."""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Retourne la somme des éléments d'une liste de floats."""
+    return sum(input_list)
