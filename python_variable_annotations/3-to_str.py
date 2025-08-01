b00ef35af8513fae2ee00644f8a62854be7e5956
voicedhealer <vivien.bernardot@gmail.com>
2025-08-01 13:47:37 +0200

    exercice 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Module qui contient une fonction to_str avec annotations de type."""
+
+
+def to_str(n: float) -> str:
+    """Retourne la représentation str d'un flottant n."""
+    return str(n)
