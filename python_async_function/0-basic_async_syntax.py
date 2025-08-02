0d24fee69ed8ccdc109a28b75ce537c837c04b54
voicedhealer <vivien.bernardot@gmail.com>
2025-08-02 10:48:12 +0200

    correction exo 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
-"""Module qui définit une coroutine wait_n, qui lance wait_random."""
-
+"""Module contenant la coroutine wait_random."""
 import asyncio
-from typing import List
+import random
 
-wait_random = __import__('0-basic_async_syntax').wait_random
-
-
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """Lance wait_random n fois en concurrence,
-    et retourne les délais triés."""
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays)
+async def wait_random(max_delay: int = 10) -> float:
+    """Attend un délai aléatoire entre 0 et max_delay (inclus), puis le retourne."""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
