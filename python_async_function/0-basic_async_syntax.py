032afed75f141349d8f2bc0fce5529de1d039fc1
voicedhealer <vivien.bernardot@gmail.com>
2025-08-02 10:49:47 +0200

    correction exo 0 V2

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,8 +3,10 @@
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
-    """Attend un délai aléatoire entre 0 et max_delay (inclus), puis le retourne."""
+    """Attend un délai aléatoire entre 0 et
+      max_delay (inclus), puis le retourne."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
