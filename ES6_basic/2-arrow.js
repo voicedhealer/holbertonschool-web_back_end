c854c2912c4c270cf7af9bcc02d00274d8a0a6a7
voicedhealer <vivien.bernardot@gmail.com>
2025-08-19 15:52:47 +0200

    Ajout de la fonction getNeighborhoodsList avec gestion des quartiers de San Francisco

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
+
+    return this.sanFranciscoNeighborhoods;
