147e3b7a425051230cb5527f9526ec1ed5845a99
voicedhealer <vivien.bernardot@gmail.com>
2025-08-20 16:32:47 +0200

    exercice 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,10 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-
     this._sqft = sqft;
-
-    if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
+    // Vérifie si la classe fille override la méthode evacuationWarningMessage
+    if (this.constructor !== Building &&
+        typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
