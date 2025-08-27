e815e32f351a9d0d2d48372292381566e475dd93
voicedhealer <vivien.bernardot@gmail.com>
2025-08-27 15:32:43 +0200

    exercice 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,19 +1,10 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map(result => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value
-        };
-      } else {
-        return {
-          status: result.status,
-          value: result.reason
-        };
-      }
-    }));
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+    })));
 }
