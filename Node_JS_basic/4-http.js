33678c167a576db7ecb518f213ee00ac9a67cf16
voicedhealer <vivien.bernardot@gmail.com>
2025-09-03 12:07:28 +0200

    update exercice

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,20 +1,10 @@
 const http = require('http');
 
-// Créer le serveur HTTP
 const app = http.createServer((req, res) => {
-  // Définir les headers de réponse
-  res.writeHead(200, { 
-    'Content-Type': 'text/plain' 
-  });
-  
-  // Envoyer la réponse
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
-// Écouter sur le port 1245 avec callback
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
-});
+app.listen(1245);
 
-// Exporter l'app
 module.exports = app;
